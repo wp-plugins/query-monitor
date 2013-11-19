027f275f27f90c626672ca3babc4eb6a687ca1c1
johnbillion <johnbillion@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-19 18:34:29 +0000

    Let's go!
    git-svn-id: https://plugins.svn.wordpress.org/query-monitor/trunk@806967 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ components/redirects.php
@@ -0,0 +1,40 @@
+<?php
+
+class QM_Component_Redirects extends QM_Component {
+
+	var $id = 'redirects';
+
+	function __construct() {
+		parent::__construct();
+		add_filter( 'wp_redirect', array( $this, 'filter_wp_redirect' ), 999, 2 );
+	}
+
+	public function filter_wp_redirect( $location, $status ) {
+
+		global $querymonitor;
+
+		if ( !$location )
+			return $location;
+		if ( !$querymonitor->show_query_monitor() )
+			return $location;
+		if ( headers_sent() )
+			return $location;
+
+		$trace = new QM_Backtrace;
+
+		header( sprintf( 'X-QM-Redirect-Trace: %s',
+			implode( ', ', $trace->get_stack() )
+		) );
+
+		return $location;
+
+	}
+
+}
+
+function register_qm_redirects( array $qm ) {
+	$qm['redirects'] = new QM_Component_Redirects;
+	return $qm;
+}
+
+add_filter( 'query_monitor_components', 'register_qm_redirects', 140 );
